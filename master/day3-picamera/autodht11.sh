742d040584c70b48ac16794fcbc139857f014dc9
odong8 <123443937+odong8@users.noreply.github.com>
2026-08-26 10:28:04 +0900

    Delete master directory

--- master/day3-picamera/autodht11.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# autodht11.py 실행 스크립트 (cron 등록용)
-#   crontab -e 에 아래처럼 등록:
-#   */10 * * * * /home/pi/master/day3-picamera/autodht11.sh >> /home/pi/master/autodht11.log 2>&1
-
-python3 /home/pi/master/day3-picamera/autodht11.py
--- master/day3-picamera/day3-cv2-version.py
+++
@@ -1,3 +0,0 @@
-import cv2 
-
-print('OpenCV 버전: ', cv2.__version__)
